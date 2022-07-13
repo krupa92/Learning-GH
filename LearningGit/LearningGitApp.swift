1467714573d8d9d5fb6ad5b841f34c597f588414
Patel Krupa <patelkrupa209@gmail.com>
2022-07-14 09:22:57 +1000

    Initial Commit

---
+++ LearningGit/LearningGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitApp.swift
+//  LearningGit
+//
+//  Created by Krupa Patel on 14/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningGitUITests/LearningGitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LearningGitUITestsLaunchTests.swift
+//  LearningGitUITests
+//
+//  Created by Krupa Patel on 14/7/2022.
+//
+
+import XCTest
+
+class LearningGitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
